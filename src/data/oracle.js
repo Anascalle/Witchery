1484aaabdb346216b0b2db91310bdd8b06823568
FuzzyDuck1930 <141763142+FuzzyDuck1930@users.noreply.github.com>
2025-03-10 11:41:23 -0500

    Add files via upload

---
+++ src/data/oracle.js
@@ -0,0 +1,17 @@
+export const preguntasOraculo = [
+    {
+        pregunta: "¿Cuál es el ingrediente principal en la poción multijugos?",
+        opciones: ["Mandrágora", "Polvo de cuerno de bicornio", "Raíz de valeriana"],
+        respuestaCorrecta: 1, // Índice de la respuesta correcta
+    },
+    {
+        pregunta: "¿Qué hechizo se usa para repeler a los dementores?",
+        opciones: ["Expelliarmus", "Expecto Patronum", "Petrificus Totalus"],
+        respuestaCorrecta: 1,
+    },
+    {
+        pregunta: "¿Cuál es el número de la Plataforma del Expreso de Hogwarts?",
+        opciones: ["9 y 3/4", "7 y 1/2", "10 y 3/4"],
+        respuestaCorrecta: 0,
+    },
+];
--- src/js/gamelogic.js
+++ src/js/gamelogic.js
@@ -1,5 +1,6 @@
 import { actualizarTurno, siguienteTurno } from "./turns.js";
-import { mostrarModal, cerrarModal, cumplioReto, noCumplioReto, mostrarModalHechizo, validarCodigo, cerrarModalHechizo} from "./modal.js";
+import { mostrarModal, cerrarModal, cumplioReto, noCumplioReto,  } from "./modal.js";
+import { mostrarModalHechizo, validarCodigo, cerrarModalHechizo, mostrarModalOraculo } from "./curses.js"
 
 document.addEventListener("DOMContentLoaded", function () {
    
@@ -19,4 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
     window.mostrarModalHechizo = mostrarModalHechizo; 
     window.validarCodigo = validarCodigo;
     window.cerrarModalHechizo = cerrarModalHechizo;
+    window.mostrarModalOraculo = mostrarModalOraculo;
+  
 });
